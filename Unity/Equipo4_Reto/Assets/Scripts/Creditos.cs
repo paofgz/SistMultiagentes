11ad344eb9042667d1cfcc86e0bd1acf24ca9cda
LeonardoMorales-coder <64058689+LeonardoMorales-coder@users.noreply.github.com>
2021-11-18 22:25:45 -0600

    unity

---
+++ Unity/Equipo4_Reto/Assets/Scripts/Creditos.cs
@@ -0,0 +1,4 @@
+﻿/*
+Agradecimientos a EVPO Games por los assets de Modular Lowpoly Streets (Free) en el siguiente link:
+https://assetstore.unity.com/packages/3d/environments/urban/modular-lowpoly-streets-free-192094
+*/

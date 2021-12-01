ff21c52960ccddab5a3c2fe38b9e9acfdf4ed795
Paola Fernandez <paolafdezgz@gmail.com>
2021-11-30 21:02:52 -0600

    Borrar unity

--- Unity/Equipo4_Reto/Assets/Scripts/Creditos.cs
+++
@@ -1,4 +0,0 @@
-﻿/*
-Agradecimientos a EVPO Games por los assets de Modular Lowpoly Streets (Free) en el siguiente link:
-https://assetstore.unity.com/packages/3d/environments/urban/modular-lowpoly-streets-free-192094
-*/
